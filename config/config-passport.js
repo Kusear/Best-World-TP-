2ffdf0eebaa26128ff222e516eae7b246fb1068e
Kusear <kusear7@gmail.com>
2021-03-08 17:52:59 +0700

    k

--- config/config-passport.js
+++ config/config-passport.js
@@ -31,7 +31,7 @@ passport.use(
         if (!user) {
           return done(null, false, { message: "Incorrect email." });
         }
-        if (!bcrypt.compare(password, user.password) /*&& password !== user.password*/) {
+        if (!bcrypt.compare(bcrypt.hash(password, 5), user.password) /*&& password !== user.password*/) {
           return done(null, false, { message: "Incorrect password." });
         }
         return done(null, user);
