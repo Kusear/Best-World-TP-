f12c520493d5ada0e0fef5d6cb4816f88eeacb7a
Kusear <kusear7@gmail.com>
2021-06-06 17:12:08 +0700

    field email confirm

--- controllers/common.js
+++ controllers/common.js
@@ -24,6 +24,7 @@ exports.login = async function (req, res) {
         _id: user._id,
         username: user.username,
         role: user.role,
+        emailConfirm: user.emailConfirm,
         token: user.getToken(),
       })
       .end();
